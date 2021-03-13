d46f85018cb6e0ebb7fbdde0df394ab31f294f91
Khiemnd <khiemnd777@gmail.com>
2021-03-13 21:39:42 +0700

    bigcities

---
+++ api/city/models/city.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {},
+    async beforeUpdate(params, data) {},
+  },
+};
