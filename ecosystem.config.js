7989a0c3588ae8c5a9ff58e3f968b01389f818df
Khiem Nguyen <khiemnd777@gmail.com>
2021-03-13 08:37:15 -0800

    pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'bigcities-api',
+      script: './server.js',
+      env: {
+        NODE_ENV: "development",
+	  },
+	  env_production: {
+		NODE_ENV: "production",
+	  },
+	},
+  ],
+};
